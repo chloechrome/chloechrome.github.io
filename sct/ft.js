f098542a7deda64cca23acfa0d48d84658a085b1
Chloe <131207798+chloechrome@users.noreply.github.com>
2025-10-13 20:48:40 +0100

    Site Version 3.1
    New repo, reupload of all files

---
+++ sct/ft.js
@@ -0,0 +1,7 @@
+function footer() {
+	const element = document.getElementById("footer");
+	element.innerHTML = '© 2024-2025 Chloe-Jane "Monochrome" - Site Version 3.1<br>| <b><a class="link" href="index.html">Home</a></b> | <b><a class="link" href="mailto:monochloe@proton.me">Contact</a></b> | Last update: 13|10|25 '
+}
+document.addEventListener("DOMContentLoaded", function() {
+    footer();
+});
