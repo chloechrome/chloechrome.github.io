74616e00d5acaeb4a5a9ea6ac980b54ededdfeaa
MonochromeHD <131207798+chloechrome@users.noreply.github.com>
2025-11-19 11:05:36 +0000

    Update ft.js

--- sct/ft.js
+++ sct/ft.js
@@ -1,8 +1,9 @@
 function footer() {
 	const element = document.getElementById("footer");
-	element.innerHTML = `© 2024-2025 Chloe-Jane "Monochrome" - Site Version 3.1<br>| <b><a class="link" href="index.html">Home</a></b> | <b><a class="link" href="mailto:monochloe@proton.me">Contact</a></b> | Last update: 08|11|25 (Aqua'	s birthday)`
+	element.innerHTML = `© 2024-2025 Chloe-Jane "Monochrome" - Site Version 3.1<br>| <b><a class="link" href="index.html">Home</a></b> | <b><a class="link" href="mailto:monochloe@proton.me">Contact</a></b> | Last update: 19|11|25`
 }
 
 document.addEventListener("DOMContentLoaded", function() {
     footer();
+
 });
